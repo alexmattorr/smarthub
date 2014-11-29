373899e8cfd9ffaece0ea1241fb4096bb7489a19
alexsmander <alexmattorr@gmail.com>
2014-11-28 19:51:25 -0800

    working on toggle function.

--- js/script.js
+++ js/script.js
@@ -1,79 +1,104 @@
-function mobileToggle() {
+function javaScript() {
 	$(document).ready(function() {
-	  $(".main-header-mobile-button").click(function(){
-	    $(".main-header-mobile").toggle();
-	  });
+
+		function mobileToggle() {
+			$(".main-header-mobile-button").click(function(){
+	    		$(".main-header-mobile").toggle();
+	  		});
+		};
+
+		function mobileColor() {
+			$(".main-header-mobile-button").click(function(){
+				$(this).toggleClass("mobile-bg-coal");
+			});
+		};
+
+		function mobileColor() {
+			$(".main-header-mobile-button").click(function(){
+				$(this).toggleClass("mobile-bg-coal");
+			});
+		};
+
+		function signinOverlay() {
+			$(".signin-overlay").click(function(){
+				$(".overlay").addClass("is-active");
+			});
+			
+			$(".overlay-button").click(function(){
+				$(".overlay").removeClass("is-active");
+			})
+		};
+
+		function smoothScroll() {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+			});
+		};
+
+		function imgSlider() {
+			$(".carousel-next").on("click", function(e) {
+				var currentActiveImage = $(".image-active");
+				var nextActiveImage = currentActiveImage.next();
+
+				if(nextActiveImage.length == 0) {
+					nextActiveImage = $(".carousel-inner img").first();
+				};
+
+				currentActiveImage.removeClass("image-active").addClass("image-hidden");
+				nextActiveImage.addClass("image-active").removeClass("image-hidden");
+				$(".carousel-inner img").not([currentActiveImage, nextActiveImage]);
+
+				e.preventDefault();
+			});
+
+			$(".carousel-previous").on("click", function(e) {
+				var currentActiveImage = $(".image-active");
+				var nextActiveImage = currentActiveImage.prev();
+
+				if(nextActiveImage.length == 0) {
+					nextActiveImage = $(".carousel-inner img").last();
+				}
+
+				currentActiveImage.removeClass("image-active").addClass("image-hidden");
+				nextActiveImage.addClass("image-active").removeClass("image-hidden");
+				$(".carousel-inner img").not([currentActiveImage, nextActiveImage]);
+
+				e.preventDefault();
+			});
+		};
+
+
+		function sectionToggle() {
+			$(".features-icon").click(function() {
+				var tabId = $("features-toggle").attr("data-tab");
+
+				$(".features-icon").removeClass("is-active");
+				$(".features-toggle").removeClass("is-active");
+
+				$(this).addClass("is-active");
+				$("#" + tabId).addClass("is-active");
+			});
+		};
+
+
+		mobileToggle();
+		mobileColor();
+		signinOverlay();
+		smoothScroll();
+		imgSlider();
+		sectionToggle();
+
 	});
 };
 
-function mobileColor() {
-	$(".main-header-mobile-button").click(function(){
-		$(this).toggleClass("mobile-bg-coal");
-	});
-};
 
-function signinOverlay() {
-	$(document).ready(function(){
-		$(".signin-overlay").click(function(){
-			$(".overlay").addClass("is-active");
-		});
-		
-		$(".overlay-button").click(function(){
-			$(".overlay").removeClass("is-active");
-		})
-	});	
-};
+javaScript();
 
-function smoothScroll() {
-	$(document).ready(function($) {
-		$(".scroll").click(function(event){		
-			event.preventDefault();
-			$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-		});
-	});
-};
 
-function imgSlider() {
-	$(document).ready(function() {
-		$(".carousel-next").on("click", function(e) {
-			var currentActiveImage = $(".image-active");
-			var nextActiveImage = currentActiveImage.next();
 
-			if(nextActiveImage.length == 0) {
-				nextActiveImage = $(".carousel-inner img").first();
-			};
 
-			currentActiveImage.removeClass("image-active").addClass("image-hidden");
-			nextActiveImage.addClass("image-active").removeClass("image-hidden");
-			$(".carousel-inner img").not([currentActiveImage, nextActiveImage]);
 
-			e.preventDefault();
-		});
 
-		$(".carousel-previous").on("click", function(e) {
-			var currentActiveImage = $(".image-active");
-			var nextActiveImage = currentActiveImage.prev();
 
-			if(nextActiveImage.length == 0) {
-				nextActiveImage = $(".carousel-inner img").last();
-			}
 
-			currentActiveImage.removeClass("image-active").addClass("image-hidden");
-			nextActiveImage.addClass("image-active").removeClass("image-hidden");
-			$(".carousel-inner img").not([currentActiveImage, nextActiveImage]);
-
-			e.preventDefault();
-		});
-	});
-};
-
-function sectionToggle() {
-	$(document).ready(function($) {
-		
-	});
-};
-mobileToggle();
-mobileColor();
-signinOverlay();
-smoothScroll();
-imgSlider();
